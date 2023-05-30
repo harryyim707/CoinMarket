c3800372bd23bbbcdda318323c4eab087bea4e9b
Hyunjun Harry Yim <harryyim707@gmail.com>
2023-05-30 21:03:33 +0900

    feat(UI, url): "/"로 들어오는 URL은 "/index"로 리디렉션, 메뉴별로 클릭하면 active요소 추가 및 삭제

--- CoinMarket/views/index.py
+++ CoinMarket/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, url_for
+from flask import Blueprint, render_template, session, url_for, redirect
 from CoinMarket.models.History import History
 from CoinMarket.models.Sell import Sell
 from flask_login import current_user
@@ -7,12 +7,17 @@ bp = Blueprint('index', __name__, url_prefix='/')
 
 
 @bp.route('/')
+def to_index():
+    return redirect(url_for('index.index'))
+
+
+@bp.route('/index')
 def index():
     session['url'] = url_for('index.index')
     last = History.query.order_by(History.id.desc()).first()
     latest_price = 0
     if last is not None:
-        sell = Sell.query.filter_by(id = last.sell_id).first()
+        sell = Sell.query.filter_by(id=last.sell_id).first()
         latest_price = sell.price
     recent_coin = Sell.query.order_by(Sell.id.desc()).filter_by(sold_out=0).first()
     if not recent_coin:
